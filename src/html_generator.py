17fac7c0da74d1d49e947acdece942853fff7e62
Brandon Fairchild <csbit32@gmail.com>
2015-08-01 12:38:07 -0400

    src/html_generator.py: Add initial functionality
    The HTML generator needs to read in a source file and iterate
    through the source.

--- src/html_generator.py
+++ src/html_generator.py
@@ -6,7 +6,24 @@ def usage():
     print('usage: html_gen [FILE]')
     sys.exit(1)
 
+def get_lines():
+    with open(sys.argv[1]) as f:
+        lines = [line for line in f]
+
+        for line in lines:
+            yield line
+
+def convert_text_to_html():
+    ''' Write the text source into HTML markup '''
+
+    with open('output.html', 'w') as out:
+        with open(sys.argv[1]) as f:
+            for line in get_lines():
+                pass
+
 if __name__ == '__main__':
 
     if len(sys.argv) == 1:
         usage()
+
+    convert_text_to_html()
