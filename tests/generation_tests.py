52def1fa32481ed40872c727930c964c1cf4ef5d
Brandon Fairchild <csbit32@gmail.com>
2015-08-13 15:52:53 -0400

    tests/generation_tests.py: Use assert_equals for equality checks

--- tests/generation_tests.py
+++ tests/generation_tests.py
@@ -2,38 +2,39 @@
 #
 # Test HTML generation
 
+from nose.tools import assert_equals
 from html_generator import Parser
 
 parser = Parser()
 
 def test_h1():
-    assert(parser.parse_line('# Header 1') == "<h1>Header 1</h1>\n")
+    assert_equals(parser.parse_line('# Header 1'), "<h1>Header 1</h1>\n")
 
 def test_h2():
-    assert(parser.parse_line('## Header 2') == "<h2>Header 2</h2>\n")
+    assert_equals(parser.parse_line('## Header 2'), "<h2>Header 2</h2>\n")
 
 def test_h3():
-    assert(parser.parse_line('### Header 3') == "<h3>Header 3</h3>\n")
+    assert_equals(parser.parse_line('### Header 3'), "<h3>Header 3</h3>\n")
 
 def test_h4():
-    assert(parser.parse_line('#### Header 4') == "<h4>Header 4</h4>\n")
+    assert_equals(parser.parse_line('#### Header 4'), "<h4>Header 4</h4>\n")
 
 def test_h5():
-    assert(parser.parse_line('##### Header 5') == "<h5>Header 5</h5>\n")
+    assert_equals(parser.parse_line('##### Header 5'), "<h5>Header 5</h5>\n")
 
 def test_h6():
-    assert(parser.parse_line('###### Header 6') == "<h6>Header 6</h6>\n")
+    assert_equals(parser.parse_line('###### Header 6'), "<h6>Header 6</h6>\n")
 
 def test_hr():
-    assert(parser.parse_line('---') == "<hr>")
+    assert_equals(parser.parse_line('---'), "<hr>")
 
 def test_start_link_generation():
-    assert(parser.parse_line('[https://www.google.com]') == \
-           "<a href='https://www.google.com'>https://www.google.com</a> ")
+    assert_equals(parser.parse_line('[https://www.google.com]'),
+                  "<a href='https://www.google.com'>https://www.google.com</a> ")
 
-    assert(parser.parse_line('[https://www.google.com] is a link') == \
-           "<a href='https://www.google.com'>https://www.google.com</a> is a link")
+    assert_equals(parser.parse_line('[https://www.google.com] is a link'),
+                  "<a href='https://www.google.com'>https://www.google.com</a> is a link")
 
 def test_embedded_link_generation():
-    assert(parser.parse_line('This is a link -> [https://www.google.com] to Google') == \
-           "This is a link -> <a href='https://www.google.com'>https://www.google.com</a> to Google")
+    assert_equals(parser.parse_line('This is a link -> [https://www.google.com] to Google'),
+                  "This is a link -> <a href='https://www.google.com'>https://www.google.com</a> to Google")
